8016e74d4d6463920c67cbe03ee0442c7bb92956
peerlink <lawrence.peerlink@gmail.com>
2018-08-29 23:31:57 +0800

    feat($export): add StartRPC func for wrap StartHTTPServer
     - TestStartRPC: assert net.Lisener.Addr
     - StartRPC: wrap StartHTTPServer

---
+++ apigateway.go
@@ -0,0 +1,66 @@
+package apigateway
+
+import (
+	"net"
+	"net/http"
+	"os"
+	"time"
+
+	cmn "github.com/DSiSc/apigateway/common"
+	"github.com/DSiSc/apigateway/log"
+	//	ctypes "github.com/DSiSc/apigateway/rpc/core/types"
+	"github.com/go-kit/kit/log/term"
+	amino "github.com/tendermint/go-amino"
+
+	rpccore "github.com/DSiSc/apigateway/rpc/core"
+	rpcserver "github.com/DSiSc/apigateway/rpc/lib/server"
+)
+
+var colorFn = func(keyvals ...interface{}) term.FgBgColor {
+	for i := 0; i < len(keyvals)-1; i += 2 {
+		if keyvals[i] == "socket" {
+			if keyvals[i+1] == "tcp" {
+				return term.FgBgColor{Fg: term.DarkBlue}
+			} else if keyvals[i+1] == "unix" {
+				return term.FgBgColor{Fg: term.DarkCyan}
+			}
+		}
+	}
+	return term.FgBgColor{}
+}
+
+func StartRPC(listenAddr string) ([]net.Listener, error) {
+
+	logger := log.NewTMLoggerWithColorFn(log.NewSyncWriter(os.Stdout), colorFn)
+
+	listenAddrs := cmn.SplitAndTrim(listenAddr, ",", " ")
+	coreCodec := amino.NewCodec()
+	// TODO(peerlink): let's see wire.go
+	//ctypes.RegisterAmino(coreCodec)
+
+	// we may expose the rpc over both a unix and tcp socket
+	listeners := make([]net.Listener, len(listenAddrs))
+	for i, listenAddr := range listenAddrs {
+		mux := http.NewServeMux()
+		rpcLogger := logger.With("module", "rpc-server")
+		wm := rpcserver.NewWebsocketManager(rpccore.Routes, coreCodec, rpcserver.ReadWait(5*time.Second))
+		// TODO(peerlink): rpcserver get eventBus from input vars.
+		//rpcserver.EventSubscriber(n.eventBus))
+		wm.SetLogger(rpcLogger.With("protocol", "websocket"))
+		mux.HandleFunc("/websocket", wm.WebsocketHandler)
+		rpcserver.RegisterRPCFuncs(mux, rpccore.Routes, coreCodec, rpcLogger)
+		listener, err := rpcserver.StartHTTPServer(
+			listenAddr,
+			mux,
+			rpcLogger,
+			// TODO(peerlink): rpcserver.Config get MaxOpenConnections from input vars.
+			//rpcserver.Config{MaxOpenConnections: maxOpenConnections},
+			rpcserver.Config{},
+		)
+		if err != nil {
+			return nil, err
+		}
+		listeners[i] = listener
+	}
+	return listeners, nil
+}
---
+++ apigateway_test.go
@@ -0,0 +1,33 @@
+package apigateway
+
+import (
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"testing"
+)
+
+var (
+	tcpAddr = "tcp://0.0.0.0:47768"
+)
+
+func TestStartRPC(t *testing.T) {
+
+	cases := []struct {
+		input   string
+		want    string
+		wanterr string
+	}{
+		{
+			input:   tcpAddr,
+			want:    "[::]:47768",
+			wanterr: "Error",
+		},
+	}
+
+	for _, tcase := range cases {
+		actualListenr, err := StartRPC(tcase.input)
+		require.Nil(t, err)
+		assert.Equal(t, tcase.want, actualListenr[0].Addr().String(), "Listener should be same")
+	}
+}
